e2b87e787462ed4ffa72a5e32322b18ae13c01b2
cahersan <cahersan@gmail.com>
2014-07-30 13:57:31 +0200

    schema_to_field, schema_to_form and field_to_schema functions written + mayor test refactoring

--- schemulator/__init__.py
+++ schemulator/__init__.py
@@ -9,7 +9,8 @@ from json_schema_toolkit.document import JSONDocument, JSONDocumentField
 Django Schemulator
 """
 
-# Dictionaries for form to schema translation {FORM : JSON_SCHEMA}
+# Dictionaries for django form fields to json schema toolkit fields translation
+# {FORM : JSON_SCHEMA}
 
 FIELDS = {
         #Django Forms built-in Field classes
@@ -62,46 +63,113 @@ KEYWORDS = {
     "choices":"content",
 }
 
-def form_to_schema(form):
-
-    class JSchema(JSONDocument):
+def field_to_schema(field):
+    """
+    """
+    field_type = field.__class__.__name__  
         
-        class Meta(object):
-            title = u'JSON Schema'
-            description = u'This is a JSON Schema describing a form'
-
-    fields = form.base_fields.values()
-    field_count = 1 # TODO: Would be better with slugyfied labels, but what if no label?
-
-    for f in fields:
-
-        field_type = f.__class__.__name__  
-        
-        mod = import_module('json_schema_toolkit.document', FIELDS[field_type])
-        jschema_field = getattr(mod, FIELDS[field_type])()
-        
-        # Special case for GenericIPAddressField, as protocol is not a field
-        # attribute, and must be induced from the validator.
-        if field_type is "GenericIPAddressField":
-            #import ipdb; ipdb.set_trace()
-            val = str(f.validators[0]) 
-            content = 'ipv6' if 'ipv6' in val else 'ipv4'
-            setattr(jschema_field, 'protocol', content)
-        
-        # Setup of JSON Schema keywords 
-        for kw in KEYWORDS.keys():
-            if hasattr(f, kw):
-                content = getattr(f, kw)
-                # Special case for choices, as these are described with a simple
-                # JSONDocumentField 
-                if kw is "choices":
-                    content = [JSONDocumentField(enum=content)]
-                setattr(jschema_field, KEYWORDS[kw], content)
-
-        # Setting the JSON Schema field within the JSON Schema
-        setattr(JSchema, "field_" + str(field_count), jschema_field)
-        field_count+=1
-
-    schema = JSchema({})._schema
+    mod = import_module('json_schema_toolkit.document', FIELDS[field_type])
+    jschema_field = getattr(mod, FIELDS[field_type])()
     
+    # Special case for GenericIPAddressField, as protocol is not a field
+    # attribute, and must be induced from the validator.
+    if field_type == "GenericIPAddressField":
+        #import ipdb; ipdb.set_trace()
+        validator = str(field.validators[0]) 
+        value = 'ipv6' if 'ipv6' in validator else 'ipv4'
+        setattr(jschema_field, 'protocol', value)
+    
+    # Setup of JSON Schema keywords 
+    for (field_kw, jschema_kw) in KEYWORDS.items():
+        if hasattr(field, field_kw):
+            value = getattr(field, field_kw)
+            # Special case for choices, as these are described with a simple
+            # JSONDocumentField 
+            if field_kw == "choices":
+                value = [JSONDocumentField(enum=value)]
+            if field_kw == "required":
+                value = not value
+            setattr(jschema_field, jschema_kw, value)
+
+    return jschema_field._generate_schema()
+
+def form_to_schema(form):
+    """
+    """
+
+    schema = {  
+        '$schema':'http://json-schema.org/draft-04/schema#',
+        'title':'JSON Schema',
+        'description':'This is a JSON Schema describing a form',
+        'properties':{}
+    }
+
+    # Loop through all form fields, get their JSON schema representation and
+    # add it to the schema properties
+    for (name, field) in form.base_fields.items():
+
+        field_schema = field_to_schema(field)
+        schema['properties'][name] = field_schema
+
     return schema
+
+def schema_to_field(schema):
+    """
+    Returns a Django Forms field when given a schema fragment describing a 
+    field: that is, any entry of the 'properties' keyword
+    """
+
+    jschema_type = schema['type']
+    kwargs = {}
+
+    # This loop sets the value of relevant field keyword arguments
+    for (field_kw, jschema_kw) in KEYWORDS.items():
+        if jschema_kw in schema:
+            value = schema[jschema_kw]
+            if jschema_kw == "optional":
+                value = not value
+            kwargs[field_kw]=value
+    
+    # This loop decides upon which form field should be used
+    if jschema_type == 'boolean':
+        field_type = 'BooleanField'  
+    elif jschema_type == 'string':
+        field_type = 'CharField'  
+        if 'format' in schema:
+            if schema['format'] == 'email':
+                field_type = 'EmailField'
+            if schema['format'] == 'date-time':
+                field_type = 'DateTimeField'
+            if schema['format'] == 'ipv4':
+                field_type = 'GenericIPAddressField'
+            if schema['format'] == 'ipv6':
+                field_type = 'GenericIPAddressField'
+                kwargs['protocol']='ipv6'
+    elif jschema_type == 'integer':
+        field_type = 'IntegerField'
+    elif jschema_type == 'number':
+        field_type = 'FloatField'
+    elif jschema_type == 'array':
+        field_type = 'ChoiceField'
+        if 'items' in schema: kwargs['choices'] = schema['items'][0]['enum']
+
+    mod = import_module('django.forms', field_type)
+    form_field = getattr(mod, field_type)
+    field = form_field(**kwargs)
+
+    return field
+
+def schema_to_form(schema):
+    """
+    """
+
+    form = forms.Form()
+
+    # loop along fields
+    for (name, prop) in schema['properties'].items():
+    
+        field = schema_to_field(prop)
+        form.fields[name] = field
+
+    return form 
+
--- schemulator/test.py
+++ schemulator/test.py
@@ -1,102 +1,299 @@
 from django.test import TestCase
 from django import forms
 
-from schemulator import form_to_schema
-        
-class TestForm(forms.Form):
-
-     boolean_field = forms.BooleanField( label="Boolean Field",
-                                         help_text="This is a boolean field",
-                                         initial=True,
-                                         required=False)
+from schemulator import form_to_schema, field_to_schema, schema_to_form, schema_to_field
  
-     text_field = forms.CharField(   label="Text Field",
-                                     help_text="This is a text field",
-                                     required=False,
-                                     max_length=100,
-                                     min_length=20)
- 
-     email_field = forms.EmailField( label="Email Field",
-                                     help_text="This is an email field",
-                                     initial="email@example.com",
-                                     max_length=100,
-                                     required=False)
- 
-     decimal_field = forms.DecimalField( label="Decimal Field",
-                                         help_text="This is a decimal field",
-                                         initial=10.04,
-                                         max_value=100,
-                                         min_value=0,
-                                         required=False)
 
-     float_field = forms.FloatField(   label="Float Field",
-                                         help_text="This is a Float field",
-                                         initial=10.04,
-                                         max_value=100,
-                                         min_value=0,
-                                         required=False)
- 
-     integer_field = forms.IntegerField( label="Integer Field",
-                                         help_text="This is an integer field",
-                                         initial=10,
-                                         max_value=100,
-                                         min_value=0,
-                                         required=False)
- 
-     choice_field = forms.ChoiceField(   label="Choice Field",
-                                         help_text="This is choice field",
-                                         choices=["choice_1", "choice_2", "choice_3"],
-                                         required=False)
+# A form to test 
+TestForm = forms.Form()
 
-     ip_field = forms.IPAddressField(   label="IP Address Field (deprecated in django 1.7)",
-                                        help_text="This is IP address field")
-                                            
-
-     #TODO: Protocol attribute can be set, but can't be retreived. Why???
-     gen_ip_field = forms.GenericIPAddressField(label="Generic IP Address Field",
-                                                help_text="This is Generic IP address field",
-                                                protocol='IPV6')
-
-     date_field = forms.DateField(  label="Date Field",
-                                    help_text="This is date field")
-
-     date_time_field = forms.DateTimeField(  label="Date-Time Field",
-                                             help_text="This is date-time field")
-
+# A schema to test
 schema = {
-    'description': 'a test schema',
-    'properties': {
-        'email': {
-            'default': None,
-            'description': 'an email field',
-            'format': 'email',
-            'null': False,
-            'optional': False,
-            'pattern': None,
-            'properties': {},
-            'title': 'email',
-            'type': 'string'
-        },
-        'text': {
-            'default': None,
-            'description':'a text field',
-            'null': False,
-            'optional': False,
-            'pattern': None,
-            'properties': {},
-            'title': 'text',
-            'type': 'string'}
-    },
-    'title': 'TestForm',
-    'type': 'object'
+    'title': u'JSON Schema',
+    'description': u'This is a JSON Schema describing a form',
+    'type': 'object', 
+    'properties': {}
+}
+
+# These are FIELDS to test within the form and their equivalent representation
+# as PROPERTIES within the schema
+
+#BOOLEAN FIELD
+boolean_field = forms.BooleanField( label="Boolean Field",
+                                    help_text="This is a boolean field",
+                                    initial=True,
+                                    required=False)
+
+boolean_field_js = {
+    'default': True,
+    'title': 'Boolean Field',
+    'description': 'This is a boolean field',
+    'type': 'boolean',
+    'optional': True,
+}
+
+# TEXT FIELD
+text_field = forms.CharField(   label="Text Field",
+                                help_text="This is a text field",
+                                required=False,
+                                max_length=100,
+                                min_length=20)
+
+text_field_js = {
+    'type': 'string',
+    'description': 'This is a text field', 
+    'title': 'Text Field',
+    'optional': True,
+    'maxLength': 100,
+    'minLength': 20,
+}
+
+# EMAIL FIELD
+email_field = forms.EmailField( label="Email Field",
+                                help_text="This is an email field",
+                                initial="email@example.com",
+                                max_length=100,
+                                required=False)
+
+email_field_js = {
+    'type': 'string', 
+    'title': 'Email Field',
+    'description': 'This is an email field', 
+    'format': 'email', 
+    'default': 'email@example.com',
+    'maxLength': 100, 
+    'optional': True,
+}
+
+# DECIMAL FIELD
+decimal_field = forms.DecimalField( label="Decimal Field",
+                                    help_text="This is a decimal field",
+                                    initial=10.04,
+                                    max_value=100,
+                                    min_value=0,
+                                    required=True)
+
+decimal_field_js = {
+    'type': 'number', 
+    'title': 'Decimal Field',
+    'description': 'This is a decimal field', 
+    'default': 10.04,
+    'maximum': 100, 
+    'minimum': 0, 
+    'optional': False,
+}
+
+# FLOAT FIELD
+float_field = forms.FloatField(   label="Float Field",
+                                    help_text="This is a float field",
+                                    initial=10.04,
+                                    max_value=100,
+                                    min_value=2.53,
+                                    required=False)
+
+float_field_js = {
+    'type': 'number', 
+    'title': 'Float Field',
+    'description': 'This is a float field', 
+    'default': 10.04,
+    'maximum': 100, 
+    'minimum': 2.53, 
+    'optional': True,
+}
+
+# INTEGER FIELD
+integer_field = forms.IntegerField( label="Integer Field",
+                                    help_text="This is an integer field",
+                                    initial=10,
+                                    max_value=50,
+                                    min_value=10,
+                                    required=False)
+
+integer_field_js = {
+    'type': 'integer', 
+    'title': 'Integer Field',
+    'description': 'This is an integer field', 
+    'default': 10,
+    'maximum': 50, 
+    'minimum': 10, 
+    'optional': True,
+}
+
+# CHOICE FIELD
+choice_field = forms.ChoiceField(   label="Choice Field",
+                                    help_text="This is a choice field",
+                                    choices=["choice_1", "choice_2", "choice_3"],
+                                    required=False)
+
+choice_field_js = {
+    'type': 'array', 
+    'title': 'Choice Field',
+    'description': 'This is a choice field', 
+    'items': [{'enum': ["choice_1", "choice_2", "choice_3"]}],
+    'optional': True,
+}
+
+# IP FIELD
+ip_field = forms.IPAddressField(   label="IP Address Field",
+                                   help_text="This is an IP address field")
+                                       
+
+ip_field_js = {
+    'type': 'string', 
+    'title': 'IP Address Field',
+    'description': 'This is an IP address field', 
+    'format': 'ipv4', 
+    'optional': False,
+}
+
+# GENERIC IP FIELD
+gen_ip_field = forms.GenericIPAddressField(label="Generic IP Address Field",
+                                           help_text="This is a Generic IP address field",
+                                           protocol='IPV6')
+
+gen_ip_field_js = {
+    'type': 'string', 
+    'title': 'Generic IP Address Field',
+    'description': 'This is a Generic IP address field', 
+    'format': 'ipv6', 
+}
+
+# DATE FIELD
+date_field = forms.DateField(  label="Date Field",
+                               help_text="This is a date field")
+
+date_field_js = {
+    'type': 'string', 
+    'title': 'Date Field',
+    'description': 'This is a date field', 
+}
+
+# DATE TIME FIELD
+date_time_field = forms.DateTimeField(  label="Date-Time Field",
+                                        help_text="This is a date-time field")
+
+date_time_field_js = {
+    'type': 'string', 
+    'title': 'Date-Time Field',
+    'description': 'This is a date-time field', 
+    'format': 'date-time', 
 }
 
 
-class FormToSchemaTestCase(TestCase):
+def dict_in_dict(d, subset_d):
+    """
+    Checks if the items in a dictionary are all contained within some other
+    dictionary. This is used to check if the schema created contains all the
+    expected information.
+    """
+    return all([i in d.items() for i in subset_d.items()])
 
-    def SetUp(self):
+class FormToSchemaTestCase(TestCase):
+    
+    def setUp(self):
+        TestForm.base_fields = {} 
+
+    def test_schema(self):
+        TestForm.base_fields = {'boolean_field':boolean_field,
+                                'text_field':text_field, }
+        schema = form_to_schema(TestForm)
+    
+    def test_boolean_field(self):
+        field_schema = field_to_schema(boolean_field)
+        self.assertTrue(dict_in_dict(field_schema, boolean_field_js))
+
+    def test_text_field(self):
+        field_schema = field_to_schema(text_field)
+        self.assertTrue(dict_in_dict(field_schema, text_field_js))
+        
+    def test_email_field(self):
+        field_schema = field_to_schema(email_field)
+        self.assertTrue(dict_in_dict(field_schema, email_field_js))
+        
+    def test_decimal_field(self):
+        field_schema = field_to_schema(decimal_field)
+        self.assertTrue(dict_in_dict(field_schema, decimal_field_js))
+        
+    def test_float_field(self):
+        field_schema = field_to_schema(float_field)
+        self.assertTrue(dict_in_dict(field_schema, float_field_js))
+        
+    def test_integer_field(self):
+        field_schema = field_to_schema(integer_field)
+        self.assertTrue(dict_in_dict(field_schema, integer_field_js))
+        
+    def test_choice_field(self):
+        field_schema = field_to_schema(choice_field)
+        choice_field_copy = choice_field_js.copy()
+        it1 = choice_field_copy.pop('items')[0]
+        it2 = field_schema.pop('items')[0]
+        self.assertTrue(dict_in_dict(it2, it1))
+        self.assertTrue(dict_in_dict(field_schema, choice_field_copy))
+        
+    def test_ip_field(self):
+        field_schema = field_to_schema(ip_field)
+        self.assertTrue(dict_in_dict(field_schema, ip_field_js))
+        
+    def test_gen_ip_field(self):
+        field_schema = field_to_schema(gen_ip_field)
+        self.assertTrue(dict_in_dict(field_schema, gen_ip_field_js))
+        
+    def test_date_field(self):
+        field_schema = field_to_schema(date_field)
+        self.assertTrue(dict_in_dict(field_schema, date_field_js))
+        
+    def test_date_time_field(self):
+        field_schema = field_to_schema(date_time_field)
+        self.assertTrue(dict_in_dict(field_schema, date_time_field_js))
+        
+
+class SchemaToFormTestCase(TestCase):
+
+    TestSchema = {}
+    
+    def setUp(self):
         pass
 
-    def test_schema_generation(self):
-        schema = form_to_schema(TestForm)
-        print schema
+    def test_boolean_field(self):
+        field = schema_to_field(boolean_field_js)
+        self.assertTrue(dict_in_dict(field_to_schema(field), boolean_field_js))
+
+    def test_text_field(self):
+        field = schema_to_field(text_field_js)
+        self.assertTrue(dict_in_dict(field_to_schema(field), text_field_js))
+        
+    def test_email_field(self):
+        field = schema_to_field(email_field_js)
+        self.assertTrue(dict_in_dict(field_to_schema(field), email_field_js))
+        
+    def test_number_field(self):
+        field = schema_to_field(float_field_js)
+        self.assertTrue(dict_in_dict(field_to_schema(field), float_field_js))
+        
+    def test_integer_field(self):
+        field = schema_to_field(integer_field_js)
+        self.assertTrue(dict_in_dict(field_to_schema(field), integer_field_js))
+        
+    def test_choice_field(self):
+        field = schema_to_field(choice_field_js)
+        field_schema = field_to_schema(choice_field)
+        choice_field_copy = choice_field_js.copy()
+        it1 = choice_field_copy.pop('items')[0]
+        it2 = field_schema.pop('items')[0]
+        self.assertTrue(dict_in_dict(it2, it1))
+        self.assertTrue(dict_in_dict(field_to_schema(field), choice_field_copy))
+        
+    def test_ipv4_field(self):
+        ip_field_js['format'] = 'ipv4'
+        field = schema_to_field(ip_field_js)
+        self.assertTrue(dict_in_dict(field_to_schema(field), ip_field_js))
+    
+    def test_ipv6_field(self):
+        ip_field_js['format'] = 'ipv6'
+        field = schema_to_field(ip_field_js)
+        self.assertTrue(dict_in_dict(field_to_schema(field), ip_field_js))
+        
+    def test_date_time_field(self):
+        field = schema_to_field(date_time_field_js)
+        self.assertTrue(dict_in_dict(field_to_schema(field), date_time_field_js))
+
