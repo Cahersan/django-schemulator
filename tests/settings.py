326468aff9f0d659f0b548366a0ca590f03b0465
cahersan <cahersan@gmail.com>
2014-08-12 14:50:08 +0200

    Removed secret key from tests/settings.py

--- tests/settings.py
+++ tests/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dpqs6adk(beo^itbfhi163z2$uf$w$@gboq&$$a!x%iw^a3+9w'
+SECRET_KEY = 'nokey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
